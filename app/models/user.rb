33a61241b4630bb903e710fc2c9520ee9a706ae7
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2020-04-20 15:46:29 +0300

    Add message resource model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  has_many :messages
+
   validates :name, presence: true, length: { minimum: 3, maximum: 255 }
   validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEXP, message: "should match required format" }
 
