262b11a84588eb67f5a6fb59130402ac6341d404
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2020-04-20 16:35:22 +0300

    Enable to login and logout users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,31 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: create_params[:email])
+
+    if user && user.authenticate(create_params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "You are successfully logged in"
+
+      redirect_to root_path
+    else
+      flash.now[:error] = "Wrong credentials"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You are logged out"
+
+    redirect_to login_path
+  end
+
+  private
+
+  def create_params
+    params[:session]
+  end
 end
